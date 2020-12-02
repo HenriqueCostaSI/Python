98281a2295b88daf4644e210d1d3d7f091f693eb
HenriqueCostaSI <henrique.mcosta@hotmail.com>
2020-12-02 20:39:20 -0300

    Estruturas Condicionais

---
+++ if_else_elif.py
@@ -0,0 +1,23 @@
+"""
+
+Estruturas Condicionais
+
+
+"""
+"""
+
+elif usado para repetições, só existe um if e um else
+
+"""
+
+idade = 6
+
+
+if idade < 18:
+    print('Menor de Idade')
+elif idade == 18:
+    print("Tem 18 anos")
+elif idade == 20
+    print('Ta velho')
+else:
+    print('Maior de idade')
