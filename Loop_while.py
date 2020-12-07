c88165d253989c68ff241b9839d029f3c6d9e178
HenriqueCostaSI <henrique.mcosta@hotmail.com>
2020-12-07 00:17:56 -0300

    Loop while

---
+++ Loop_while.py
@@ -0,0 +1,24 @@
+"""
+Loop While 
+
+while expressão booleana
+
+O bloco while do while será repetido enquanto a expressão for verdadeiro
+
+
+"""
+
+num = 1
+
+while num < 10:
+    print(num)
+    num += 1
+
+
+#Exemplo 2
+
+resposta = ''
+
+while resposta != 'sim':
+    resposta = input('Já acabou Jéssica: ')
+
