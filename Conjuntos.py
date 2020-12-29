3157744e8ada9a5f3a0a5706bfa3d1d5629fb6a7
HenriqueCostaSI <henrique.mcosta@hotmail.com>
2020-12-28 23:59:45 -0300

    Conjuntos

--- Conjuntos.py
+++ Conjuntos.py
@@ -22,7 +22,7 @@ Diferença entre Conjuntos (set) e Mapas (Dicionários) em Python:
 
 # Forma 1
 
-s = set({1, 1, 2, 3, 4, 24, 6, 43})##Números repetidos são ignorados
+s = set({1, 1, 2, 3, 4, 24, 6, 43})  # Números repetidos são ignorados
 
 print(s)
 print(type(s))
@@ -36,7 +36,7 @@ print(type(s2))
 
 ############
 
-#Listas, Tuplas, >> set
+# Listas, Tuplas, >> set
 
 #set(Listas, Tuplas)
 
@@ -48,27 +48,27 @@ else:
 
 # Importante lembrar que, além de não termos valores duplicados, não temos ordem
 
-lista =[99, 2, 34, 23, 2, 34, 12, 1 , 44, 5]
+lista = [99, 2, 34, 23, 2, 34, 12, 1, 44, 5]
 print(f'Lista: {lista}')
 
-tupla = (99, 2, 34, 23, 2, 34, 12, 1 , 44, 5)
+tupla = (99, 2, 34, 23, 2, 34, 12, 1, 44, 5)
 print(f'Tupla: {tupla}')
 
-dicionario =  {}.fromkeys(lista, 'dict')
-print(f'Dicionário: {dicionario}')#Dicionarios não aceitam chaves duplicadas
+dicionario = {}.fromkeys(lista, 'dict')
+print(f'Dicionário: {dicionario}')  # Dicionarios não aceitam chaves duplicadas
 
-conjunto = {99, 2, 34, 23, 2, 34, 12, 1 , 44, 5}
-print(f'Conjunto: {conjunto}')#Conjuntos não aceitam valores duplicados
+conjunto = {99, 2, 34, 23, 2, 34, 12, 1, 44, 5}
+print(f'Conjunto: {conjunto}')  # Conjuntos não aceitam valores duplicados
 
 ################################
 
-#Podemos misturar os tipos de dados em um set
+# Podemos misturar os tipos de dados em um set
 
 s3 = {1, 'preço', True, 32.22}
 
 print(s3)
 
-#interar
+# interar
 
 for valores in s3:
     print(valores)
@@ -76,8 +76,102 @@ for valores in s3:
 
 ################################
 
-#Usos interessantes com sets
+# Adicionando elementos em um conjunto
+
+cidades = ['Belo Horizonte', 'Campo Grande', 'Cuiaba', 'São Paulo']
+
+cidades.add(4)
+cidades.add(4)  # DUplicidade não gera erro
+
+# removendo valores
+
+# Forma 1
+cidades.remove('Belo Horizonte')
+
+# Forma 2
+cidades.discard(4)
+
+################################
 
 
+# COPIANDO
+
+# Forma 1 - DeepcoPy
+
+novo = cidades.copy()
+
+novo.add(5)
+
+print(novo)
+print(cidades)
+
+# Forma 2 -> Shallow copy
+
+novo = cidades
+
+novo.add(5)
+
+print(novo)
+print(cidades)
+
+############
+
+novo.clear()
+
+################################
+
+# Métodos Matemáticos
+
+estudantes_python = {'Henrique', 'Ana', 'João', 'Pedro'}
+
+java_students = {'Fernando', 'Ana', 'Gabriela', 'Marcos'}
+
+# União entre dois conjuntos
+
+# Forma 1
+uniao = estudantes_python.union(java_students)
+
+print(uniao)
+
+# Forma 2 -> |
+
+uniao2 = java_students | (estudantes_python)
 
 
+""""""""
+
+# Intersecção
+
+# Forma 1
+ambos = estudantes_python.intersection(java_students)
+
+print(ambos)
+
+# Forma 2 -> &
+
+ambos2 = java_students & estudantes_python
+
+print(ambos2)
+
+
+""""""""
+
+# Diferença
+
+so_python = estudantes_python.difference(java_students)
+
+print(so_python)
+
+so_java = java_students.difference(estudantes_python)
+
+print(so_java)
+
+
+################################
+
+s = {1, 1, 2, 3, 4, 24, 6, 43}
+
+print(sum(s))
+print(max(s))
+print(min(s))
+print(len(s))
