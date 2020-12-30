d414d09f0da96e39af3e42fb85362b38a5e7eb83
HenriqueCostaSI <henrique.mcosta@hotmail.com>
2020-12-30 01:37:00 -0300

    deque

---
+++ Deque.py
@@ -0,0 +1,32 @@
+"""
+Deque -> Podemos dizer que deque é uma lista de alta performance 
+
+"""
+
+# Importa
+
+from collections import deque
+
+# Criando deque 
+
+deq = deque('geek')
+
+print(deq)
+
+# Adicionando elementos no deque
+
+deq.append('y') # Adiciona no final
+
+deq.appendleft('k') # Adiciona no começo 
+
+# remover
+
+print(deq.pop())
+
+print(deq)
+
+print(deq.popleft()) # Remove e retorna o primeiro elemento
+
+print(deq)
+
+
