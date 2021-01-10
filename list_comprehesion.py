5661bfd59c37cadff54aac63f3c79796ed89492d
HenriqueCostaSI <henrique.mcosta@hotmail.com>
2021-01-10 02:38:38 -0300

    List Comprehension

--- list_comprehesion.py
+++ list_comprehesion.py
@@ -122,3 +122,8 @@ pares = [numero for numero in numeros if not numero % 2]
 
 print(pares)
 print(impares)
+
+# 2
+
+res = [numero * 2 if numero % 2 == 0 else numero / 2 for numero in numeros]
+print(res)
