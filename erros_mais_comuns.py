af2c906129dea8c21c81ff22b9a5cea97a6410c8
HenriqueCostaSI <henrique.mcosta@hotmail.com>
2021-03-27 22:29:15 -0300

    Erros mais comuns

---
+++ erros_mais_comuns.py
@@ -0,0 +1,24 @@
+"""
+Erros Mais Comuns
+
+Os erros mais comuns:
+
+-> SystaxError
+
+-> NameError: variável ou função não definada
+
+-> TypeError: Ocorre quando uma função/operação/ação é aplicada a um tipo errado
+
+-> IndexError: Ocorre quando tentamos acessar um elemento em uma lista ou outro tipo de dado indexado utilizando um indice invalido.
+
+-> ValueError: Ocorre quando uma função build in (integrada) recebe um argumento com tipo correto mas de valor inapropriado
+
+-> KeyError: Ocorre quando tentamos acessar um dicionário com uma chave que não existe.
+
+-> AttributeError: Ocorre quando uma variável não tem um atributo/função
+
+-> IndentationError: Ocorre quando não respeitamos a indentação do Python (4 espaços)
+
+OBS: Exeptions e Erros são sinônimos na programação
+
+"""
